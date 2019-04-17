ed875104690e8857f2aa1abe9bbcd20be7c14b47
Rebecca Pae <r.pae92@gmail.com>
2019-04-16 22:41:00 -0700

    added functionality for adding buttons

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
+// Test Global Variables (may not need later)
+var clicked = false;
+
 // Array containing all the animals
-var animalsArray = [];
+// var animalsArray = [];
 
 // Capture the user's input into a variable when user clicks submit
 $("#submit").on("click", function() {
     event.preventDefault();
     var userAnimal = $("#inputAnimal").val();
-    animalsArray.push(userAnimal);
-    console.log(animalsArray);
+    // animalsArray.push(userAnimal);
+    // console.log(animalsArray);
     console.log("userAnimal: " + userAnimal)
+
     //Clear the input field
     $("#inputAnimal").val("")
 
     // Make each animal into a button and append it to the animalButtons div 
     var animalButton = $("<button>");
     animalButton.attr("animal", userAnimal);
-    animalButton.addClass("btn btn-light");
+    animalButton.addClass("btn btn-light animalButton");
     animalButton.text(userAnimal);
     $("#animalButtons").append(animalButton);
+
+    animalButtonClicked();
 });
 
 // When the user clicks on a button, run the AJAX function
-$(".btn btn-light").on("click", function() {
+function animalButtonClicked() {
+    $(".animalButton").on("click", function() {
+    clicked = true;
+    console.log(clicked);
     var animal = $(this).attr("animal");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -30,7 +39,23 @@ $(".btn btn-light").on("click", function() {
         method: "GET"
     })
         .then(function(response) {
+        console.log(response);
         var results = response.data;
         console.log(results);
+        
+            // For loop to create and append the gifs to the page
+            for (i = 0; i < 11; i++) {
+            var imgDiv = $("<img>");
+            imgDiv.attr({"src": response.data[i].images.fixed_height_still, "data-still": response.data[i].images.original_still, "data-animate": response.data[i].images.original_mp4, "data-state": "still"});
+            imgDiv.addClass("gif");
+            $("#gifDiv").append(imgDiv);
+            };
+
+            
+
         });
-});
+    });
+};
+
+// Run this functino for the first three pre-populated animal buttons
+animalButtonClicked();
