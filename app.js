cfc5f83704924e561fa91041cace54c2986d53ef
Rebecca Pae <r.pae92@gmail.com>
2019-04-17 20:57:40 -0700

    added all jquery function and css styling

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// Test Global Variables (may not need later)
+// Global Variable s
 var clicked = false;
-
-// Array containing all the animals
-// var animalsArray = [];
 
 // Capture the user's input into a variable when user clicks submit
 $("#submit").on("click", function() {
@@ -31,27 +28,53 @@ function animalButtonClicked() {
     clicked = true;
     console.log(clicked);
     var animal = $(this).attr("animal");
+    console.log(animal);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
         .then(function(response) {
         console.log(response);
         var results = response.data;
         console.log(results);
-        
+        $("#gifDiv").empty();
+
             // For loop to create and append the gifs to the page
-            for (i = 0; i < 11; i++) {
+            for (i = 0; i < 10; i++) {
             var imgDiv = $("<img>");
-            imgDiv.attr({"src": response.data[i].images.fixed_height_still, "data-still": response.data[i].images.original_still, "data-animate": response.data[i].images.original_mp4, "data-state": "still"});
+            imgDiv.attr({"src": response.data[i].images.original_still.url, "data-still": response.data[i].images.original_still.url, "data-animate": response.data[i].images.original.url, "data-state": "still"});
             imgDiv.addClass("gif");
             $("#gifDiv").append(imgDiv);
+
+            // To include the rating
+            var rating = response.data[i].rating
+            console.log(rating);
+            var pRating = $("<p>");
+            pRating.text("Rating: " + rating);
+            $("#gifDiv").append(pRating);
+
             };
 
-            
+            // To pause/unpause a gif for a click event
+            $(".gif").on("click", function() {
+            console.log("gif clicked");
+            var state = $(this).attr("data-state");
+
+            if(state == "still") {
+                console.log("still state: " + state)
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                console.log("animate state: " + state);
+                $(this).attr("data-state", "still");
+            };
+
+            });
 
         });
     });
@@ -59,3 +82,4 @@ function animalButtonClicked() {
 
 // Run this functino for the first three pre-populated animal buttons
 animalButtonClicked();
+
